51cb6a59d7f6164dd4696f24081dd16c6148eabe
2020070620 <2020070620@student.pppkpetra.sch.id>
2025-11-17 04:34:17 +0700

    Add files via upload

---
+++ ecommerce.sql
@@ -0,0 +1,123 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Waktu pembuatan: 16 Nov 2025 pada 21.33
+-- Versi server: 8.0.43
+-- Versi PHP: 8.2.29
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `ecommerce`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `register_login`
+--
+
+CREATE TABLE `register_login` (
+  `id` int NOT NULL,
+  `email` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `username` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` varchar(100) COLLATE utf8mb4_general_ci NOT NULL,
+  `name` varchar(100) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `phone` varchar(20) COLLATE utf8mb4_general_ci NOT NULL,
+  `address` text COLLATE utf8mb4_general_ci NOT NULL,
+  `birth` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `register_login`
+--
+
+INSERT INTO `register_login` (`id`, `email`, `username`, `password`, `name`, `phone`, `address`, `birth`) VALUES
+(9, 'jason@gmail.com', '123qwe', '$2y$10$jxJz.B4p2cxHvME3A9UobO.XPdk8eSuCsJ6wdWetJLORDgDvKYZ4e', 'qwerty', '+62323432454354543', 'qwerty', '2025-11-30'),
+(10, 'j4s@gmail.com', 'zxc', '$2y$10$MxZa6rhQP6g663fh4q4JweyZ03tpgejtDD2WB.UrN7IRVy.KqAjZe', 'zxc', '+6255555555', 'cvbvcb', '2025-11-30');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `transaksi`
+--
+
+CREATE TABLE `transaksi` (
+  `transaksi_id` int NOT NULL,
+  `username` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `phone` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `address` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `product` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `jumlah` int NOT NULL,
+  `total` int NOT NULL,
+  `payment_method` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `tanggal` datetime DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data untuk tabel `transaksi`
+--
+
+INSERT INTO `transaksi` (`transaksi_id`, `username`, `phone`, `address`, `product`, `jumlah`, `total`, `payment_method`, `tanggal`) VALUES
+(1, '123qwe', '+62323432454354543', 'qwerty', 'Garlickoe Bawang Hitam Tunggal 250gr', 13, 1169987, 'COD', '2025-11-15 21:08:16'),
+(2, 'zxc', '+6255555555', 'cvbvcb', 'Garlickoe Bawang Hitam Tunggal 1kg', 16, 5760000, 'Transfer Bank', '2025-11-17 03:12:32'),
+(3, 'zxc', '+6255555555', 'cvbvcb', 'Garlickoe Bawang Hitam Tunggal 1kg', 16, 5760000, 'Transfer Bank', '2025-11-17 03:13:22');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `register_login`
+--
+ALTER TABLE `register_login`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username` (`username`);
+
+--
+-- Indeks untuk tabel `transaksi`
+--
+ALTER TABLE `transaksi`
+  ADD PRIMARY KEY (`transaksi_id`),
+  ADD KEY `fk_transaksi_username` (`username`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `register_login`
+--
+ALTER TABLE `register_login`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT untuk tabel `transaksi`
+--
+ALTER TABLE `transaksi`
+  MODIFY `transaksi_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
+--
+
+--
+-- Ketidakleluasaan untuk tabel `transaksi`
+--
+ALTER TABLE `transaksi`
+  ADD CONSTRAINT `fk_transaksi_username` FOREIGN KEY (`username`) REFERENCES `register_login` (`username`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
